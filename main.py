f06c082eb9ab5eec544b8186febcd008f767eb24
LeonKote <roman892265@gmail.com>
2023-10-30 20:03:00 +0500

    added new feature

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import numpy as np
 # Генерируем значения x от -2π до 2π
 x = np.linspace(-2*np.pi, 2*np.pi, 1000)
 
-# Вычисляем значения функции sin(x) * cos(x)
-y = np.sin(x) * np.cos(x)
+# Вычисляем значения функции sin(x) + cos(x)
+y = np.sin(x) + np.cos(x)
 
 # Создаем график
 plt.figure(figsize=(8, 6))
